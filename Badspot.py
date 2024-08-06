ee0ca603a5f7c83b972ed0d2f63d31b3571c26db
Benedictus Briatore Ananta <109423805+benedictusbriatoreananta@users.noreply.github.com>
2024-08-06 07:14:44 +0700

    Badspot.py

--- Badspot.py
+++ Badspot.py
@@ -10,6 +10,9 @@ from google.cloud import storage
 import json
 import os
 
+# Set environment variable for Google Cloud credentials
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'path/to/your/credentials.json'
+
 # Hide Streamlit style
 hide_st_style = """
                 <style>
@@ -48,25 +51,6 @@ with st.sidebar:
         menu_icon="broadcast tower",
         default_index=0,
     )
-
-# Fungsi untuk menghubungkan ke GCP
-def connect_to_gcp():
-    credentials_json = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
-    if credentials_json is None:
-        st.error("Environment variable 'GOOGLE_APPLICATION_CREDENTIALS_JSON' is not set.")
-        return None
-    
-    credentials_dict = json.loads(credentials_json)
-    client = storage.Client.from_service_account_info(credentials_dict)
-    return client
-
-# Contoh penggunaan fungsi connect_to_gcp
-client = connect_to_gcp()
-if client is None:
-    st.stop()  # Stop the app if the client is not created
-
-bucket_name = 'badspot_predict'
-bucket = client.get_bucket(bucket_name)
 
 # Define load_model function from GCS
 def load_model(bucket_name, model_path, scaler_path, encoder_path, credentials_json):
@@ -205,19 +189,7 @@ if selected == "Predictions":
                         scaler_path = 'models/scaler.pkl'
                         encoder_path = 'models/label_encoder.pkl'
 
-                        credentials_json = {
-                          "type": "service_account",
-                          "project_id": "badspot-predict",
-                          "private_key_id": "f845ec177bda10934cc4c706609ccf8cc72abc62",
-                          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDCmZl/DmpRHdaV\nMDPFLLmS76DLtOWgNAYAcLNgVItRx/3Zh9nY/usARt8/yNUXOmpjhVyJqpcTB6j5\n8mJsOof2vN0c2raqgU3fv3QgMiwhgzldZOjht52jgNgg3sJQIQCqrIuGUhaK6qFS\n8+HCLvPgA4pfHtc1mpOXCB8ntUheySCLqLMprWQSrOqXzXpLznZY6Thy3wEZ1nZS\n7Mn0xEcV4xu113D+JPi/GTCD5e622nuCk/vegieKWOI1/3ZAHksphDowuRKAXp5B\nbhZ60Te5P6SsGKUzAc5u5+Ds1cisEtUWUh+Dbx6EmuVqyayW/buXnGonrixFD76m\n4JbG/H5JAgMBAAECggEAO1qIA8XMzaLxMR27nJcwns1x2M/6/asAbZ/yRh5jQ/Vg\nyrlxy4qs+K+lJhpGTEhn7KTffanHXGmIs3unY2VS1QHz2vomnsjDjMwjSjKjXTFe\nDMtbcIUFkroYsaXf1HrMbkIkaDqfX42C2P9Dy2twvNG3oZ2RfGsCOx2iCwVy5l2e\n5nE3H+UtpENUjlQmGUdvx0Gt4btaTAXoB6u8uGVodUka2w6uJQRxNj7rUjT+AYOF\n3rsDcSoAkgTBiFQD25gixmrlpoGI26i5R7ZgkdZWzhKB55eTinV08TAMyPB6hqJu\nNZAzsgY17VU8l2iqILzAroWxkBOzoSUw1x9eXUdKFQKBgQD72nVE4sIiZEYaanuj\n8H4vMf7C0dvidyU1lMzkfOPBOoBoHIcC7HB9ibz5lolY9/+1Qpb0r7CI1Ty+moXw\n+P9MB90cM2LvSfFeiAn5OD1cHRE8uLprVumz/uDdyDQj+jpyFPWARtsNJGnD2IlE\n/qz919iJ6UvLTd+bW7B3lbaKiwKBgQDFzdK69tUoVJlW3DqwhOq0XJJOjKVfKleN\nO1RI86E+wZW/pfKneWVoXEd1uzfTdMjgyGxeyylaAp0tAbGJR1N5ovT4ZVCCtlYH\n0Nujigd8A1aH4+DRV0xGJxDV6+sfy2ifJbPzLxS6YiLxIHUs9Y1dSGye109QFn2i\nK0hvqeT4+wKBgE86vVOLZnk9RFSBFR5QxNGCD9wn+t12j+0YP5DNvTSHe+fEubBw\nwz6q+xklg1XKxtW1+hlFv+p78p0frW1OV7oKa0O44rHWeCk98K1HRP5aYpbQokys\nTd8DGqiKl+SNjp9e+pB9OeIbh25GC4D6AV/l7EgObXqqp+KO6KfmIEPbAoGAeNiJ\nYS5CBhTBZd7AgG2EEtLnk9O7iMuOl7tif/tQTM3qVh7lg8nX2Y1fHx9VOPwFacco\n5jUKu3ITYpbBR1RrPEoBjewf5uvM3ZONTHmcnvhPGlBvXpYOBGIDmB49FLyp22km\nanEeIcyo+lXRILmYNyBzNAvo6c4DzVrSM15BhJcCgYEAsNbqypWEAYzN3d0YKy0d\n3yqYPzGct2y0icT5VLmfWdWdMfSluVtrOf9B99mySKe3eXD29Hqtdn3uOWa/ZPEn\nB5fFNO7GNh14kKBcPOq3gJkjAKaCSvZJ6V/AIigiQLoAhnBJX2L8uF21gaGV7AmC\nAWClSA+65FAVRUJOtDRwW3s=\n-----END PRIVATE KEY-----\n",
-                          "client_email": "streamlit-account-service@badspot-predict.iam.gserviceaccount.com",
-                          "client_id": "114756292400923299842",
-                          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                          "token_uri": "https://oauth2.googleapis.com/token",
-                          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/streamlit-account-service%40badspot-predict.iam.gserviceaccount.com",
-                          "universe_domain": "googleapis.com"
-                        }
+                        credentials_json = json.loads(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
 
                         model, scaler, label_encoder = load_model(bucket_name, model_path, scaler_path, encoder_path, credentials_json)
 
@@ -239,11 +211,8 @@ if selected == "Contributors":
         st.divider()
         st.markdown('''
         ####
-        - **Benedictus Briatore Ananta**: Lead Data Scientist
-        - **Team Member 2**: Role
-        - **Team Member 3**: Role
-        - **Team Member 4**: Role
-        
+        - **Benedictus Briatore Ananta**
+    
         ### Acknowledgments:
-        We would like to express our gratitude to all those who have contributed to this project. Special thanks to our mentors, peers, and the open-source community for their invaluable support and resources.
+        We would like to express our gratitude to all those who have contributed to this project. Special thanks to our mentors, peers, and the open-source community for their invaluable support and contributions.
         ''')
